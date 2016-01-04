de96bccf306dfa6d9d1c3c29b46fcae5d92de33c
Dianna <dhummel1210@gmail.com>
2016-01-04 16:03:23 -0500

    added while loop to intialize game

--- numGame.py
+++ numGame.py
@@ -1,1 +1,16 @@
 import random
+
+#generate random number between 1 - 10
+random_num = random.randint(1,10)
+
+while True:
+    #get a number guess from player
+    guess = int(input('Pick a number between 1 and 10...'))
+    if random_num == guess:
+        print('You guessed correctly!')
+        break
+    else:
+        print('Try again...')
+
+#compare guess to secret number
+#print hit or miss
